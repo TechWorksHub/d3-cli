8701d28d38d1e0a3560eab9eb82ffbf1e4543513
Ash Setter <ash@nquiringminds.com>
2022-09-20 14:31:16 +0100

    feat: retrieve and build malicious url's behaviour

--- d3-cli/src/d3_scripts/d3_build.py
+++ d3-cli/src/d3_scripts/d3_build.py
@@ -12,8 +12,11 @@ from .yaml_tools import is_valid_yaml_claim, get_yaml_suffixes, load_claim
 from .claim_graph import build_claim_graph
 from .build_type_map import build_type_map
 from .d3_build_vulnerabilities import build_vulnerabilities
+from .d3_build_malicious_behaviours import get_malicious_behaviours
 from .json_tools import write_json
 import typing
+from tempfile import TemporaryDirectory
+import yaml
 
 
 class PathFinder:
@@ -23,6 +26,7 @@ class PathFinder:
 
     def add_to_d3_map(self, claim_filepath, folder):
         claim_relative_filepath = claim_filepath.relative_to(folder)
+        print(claim_relative_filepath)
         json_filepath = Path(
             self.output_dir, str(claim_relative_filepath).replace(".yaml", ".json")
         )
@@ -94,6 +98,23 @@ def d3_build(
     files_to_process = [file for file in files_to_process if file]
     pbar.update(15)
 
+    # retrieve malicious malware urls and add malicious behaviours
+    pbar.set_description("Retrieving malicious URLs")
+    malicious_behaviours = get_malicious_behaviours()
+    malicious_behaviours_dir = TemporaryDirectory()
+    malicious_behaviours_dir.path = Path(malicious_behaviours_dir.name)
+    folder = malicious_behaviours_dir.path / "maliciousUrls"
+    folder.mkdir(parents=True, exist_ok=True)
+    for malicious_behaviour in malicious_behaviours:
+        id = malicious_behaviour["id"]
+        behaviour_filepath = folder / f'mal-{id}.behaviour.d3.yaml'
+        behaviour_yaml = {"type": "d3-device-type-behaviour", "credentialSubject": malicious_behaviour}
+        with open(behaviour_filepath, 'w') as outfile:
+            yaml.dump(behaviour_yaml, outfile, default_flow_style=False)
+        pathFinder.add_to_d3_map(behaviour_filepath, malicious_behaviours_dir.path)
+        files_to_process.append(str(behaviour_filepath))
+    pbar.update(10)
+
     pbar.set_description("Loading claims")
     behaviour_files = get_files_by_type(files_to_process, "behaviour")
     behaviour_jsons = tuple(pool.map(load_claim, behaviour_files))
@@ -121,7 +142,7 @@ def d3_build(
     check_guids(guids, files_to_process)
     parent_guids = list(pool.map(get_parent_claims, claim_jsons))
     check_guids_array(parent_guids, files_to_process)
-    pbar.update(20)
+    pbar.update(10)
 
     # Pass behaviour files into process_claim_file function
     pbar.set_description("Finding inherited rules & checking for vulnerabilities")
@@ -146,6 +167,7 @@ def d3_build(
         for warning in warnings:
             logging.warning(f"{warning} in {files_to_process[i]}")
 
+    malicious_behaviours_dir.cleanup()
     pool.close()
     pbar.update(20)
     pbar.set_description("Done!")
