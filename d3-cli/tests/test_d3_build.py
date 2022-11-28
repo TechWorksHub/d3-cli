05e8b60735e40464e495c2e47bc28a99cf3bd36c
Ash Setter <ash@nquiringminds.com>
2022-11-28 11:04:41 +0000

    test: Added test for duplicate rulename on inheritance from parent

--- d3-cli/tests/test_d3_build.py
+++ d3-cli/tests/test_d3_build.py
@@ -1,6 +1,6 @@
 import pytest
 from pathlib import Path
-
+import json
 import d3_scripts.d3_build
 
 
@@ -57,7 +57,8 @@ def test_invalid_uri(caplog):
 
 def test_non_existent_parent_behaviour():
     """Test whether behaviours with non-existent parents raises an error"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "non-existent-parent-behaviour"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "non-existent-parent-behaviour"
     output_dir = test_dir / "json"
     with pytest.raises(Exception) as excinfo:
         d3_scripts.d3_build.d3_build(
@@ -71,7 +72,8 @@ def test_non_existent_parent_behaviour():
 
 def test_circular_behaviour_dependencies():
     """Test whether behaviours with circular parent dependencies raise an error"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "circular-type-dependence"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "circular-type-dependence"
     output_dir = test_dir / "json"
     with pytest.raises(Exception) as excinfo:
         d3_scripts.d3_build.d3_build(
@@ -84,7 +86,8 @@ def test_circular_behaviour_dependencies():
 
 def test_non_existent_parent_type():
     """Test whether types with non-existent parents raises an error"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "non-existent-parent-type"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "non-existent-parent-type"
     output_dir = test_dir / "json"
     with pytest.raises(Exception) as excinfo:
         d3_scripts.d3_build.d3_build(
@@ -98,7 +101,8 @@ def test_non_existent_parent_type():
 
 def test_duplicate_property_type_inheritance():
     """Test whether inheriting duplicate properties from types raises an error"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "duplicate-property-type-inheritance"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "duplicate-property-type-inheritance"
     output_dir = test_dir / "json"
     with pytest.raises(Exception) as excinfo:
         d3_scripts.d3_build.d3_build(
@@ -111,7 +115,8 @@ def test_duplicate_property_type_inheritance():
 
 def test_inherit_missing_property():
     """Test whether attempting to inherit missing properties from parent types raises an error"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "missing-property-type-inheritance"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "missing-property-type-inheritance"
     output_dir = test_dir / "json"
     with pytest.raises(Exception) as excinfo:
         d3_scripts.d3_build.d3_build(
@@ -138,7 +143,8 @@ def test_firmware_with_missing_type():
 
 def test_duplicate_property_type_inheritance_single_parent():
     """Test whether inheriting duplicate properties from a single parent type is ignored"""
-    test_dir = Path(__file__).parent / "__fixtures__" / "duplicate-property-type-inheritance-single-parent"
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "duplicate-property-type-inheritance-single-parent"
     output_dir = test_dir / "json"
     # should succeed
     d3_scripts.d3_build.d3_build(
@@ -158,6 +164,24 @@ def test_cpe_resoolves():
         output_dir=output_dir,
         skip_mal=True,
     )
+
+
+def test_inherited_rulename_conflict():
+    """Test whether behaviour inheriting a rule with duplicate name to a behaviour rule resolves without duplication"""
+    test_dir = Path(__file__).parent / "__fixtures__" / \
+        "inherited-rulename-conflict"
+    output_dir = test_dir / "json"
+    # should succeed
+    d3_scripts.d3_build.d3_build(
+        d3_folders=[test_dir],
+        output_dir=output_dir,
+        skip_mal=True,
+    )
+    # should not have duplicate rule names
+    for json_file in (output_dir).glob("*behaviour.d3.json"):
+        with json_file.open() as f:
+            data = json.load(f)
+        assert len(data["credentialSubject"]["rules"]) == len(set(rule["name"] for rule in data["credentialSubject"]["rules"]))
 
 
 def test_build():
